fcc716b4911b22f60f20feeab4141b31d8b4e581
muhammadnuman-eng <numandev304@gmail.com>
2025-07-29 10:57:35 -0700

    first commit

--- clinic-frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-// src/App.js
-import React from 'react';
-import Dashboard from './components/Pages/Dashboard';
-
-function App() {
-  return <Dashboard />;
-}
-
-export default App;
--- clinic-frontend/src/utils/formatDate.js
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs';
-
-export const formatAppointmentTime = (time) => {
-  return dayjs(time).format('YYYY-MM-DD HH:mm');
-};
